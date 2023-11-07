b18c0e58429a3cea7d56f0c58df28a1ea9338bfe
ShailiSahrawat <91409448+ShailiSahrawat@users.noreply.github.com>
2023-11-07 12:27:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,426 @@
+let myjokes = [
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 12,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 18,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Saying that Java is nice because it works on every OS is like saying that anal sex is nice because it works on every gender.",
+      "flags": {
+        "nsfw": true,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+      },
+      "id": 30,
+      "safe": false,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Hey Girl,\nRoses are #ff0000,\nViolets are #0000ff,\nI use hex codes,\nBut I'd use RGB for you.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 41,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "\"We messed up the keming again guys.\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 20,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "A SQL statement walks into a bar and sees two tables.\nIt approaches, and asks \"may I join you?\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 5,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Two SQL tables sit at the bar. A query approaches and asks \"Can I join you?\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 221,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Documentation is like sex:\nWhen it's good, it's very good.\nWhen it's bad, it's better than nothing...",
+      "flags": {
+        "nsfw": true,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "safe": false,
+      "id": 305,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "\"Can I tell you a TCP joke?\"\n\"Please tell me a TCP joke.\"\n\"OK, I'll tell you a TCP joke.\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 57,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "A byte walks into a bar looking miserable.\nThe bartender asks it: \"What's wrong buddy?\"\n\"Parity error.\" it replies. \n\"Ah that makes sense, I thought you looked a bit off.\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 24,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "UDP is better in the COVID era since it avoids unnecessary handshakes.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 259,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Dark",
+      "type": "single",
+      "joke": "I'll never forget my Granddad's last words to me just before he died. \"Are you still holding the ladder?\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 208,
+      "safe": false,
+      "lang": "en"
+    },
+    {
+      "category": "Misc",
+      "type": "single",
+      "joke": "My wife is really mad at the fact that I have no sense of direction.\nSo I packed up my stuff and right.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 201,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Debugging is like being the detective in a crime movie where you're also the murderer at the same time.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 42,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "The glass is neither half-full nor half-empty, the glass is twice as big as it needs to be.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 23,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Eight bytes walk into a bar.\nThe bartender asks, \"Can I get you anything?\"\n\"Yeah,\" reply the bytes.\n\"Make us a double.\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 34,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Pun",
+      "type": "single",
+      "joke": "How do you make holy water? You boil the hell out of it.",
+      "flags": {
+        "nsfw": false,
+        "religious": true,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 202,
+      "safe": false,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "I've got a really good UDP joke to tell you but I don’t know if you'll get it.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 0,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Dark",
+      "type": "single",
+      "joke": "I hate double standards. Burn a body at a crematorium, you're \"being a respectful friend.\" Do it at home and you're \"destroying evidence.\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+      },
+      "safe": false,
+      "id": 274,
+      "lang": "en"
+    },
+    {
+      "category": "Misc",
+      "type": "single",
+      "joke": "What does the MacBook have in common with Donald Trump?\n\nI would tell you....\nBut I don't compare apples to oranges.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": true,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 233,
+      "safe": false,
+      "lang": "en"
+    }
+  
+  
+  
+  ]
+  
+  // let index = Math.floor(Math.random() * (myjokes.length - 1))
+  // console.log(index)
+  // joke.innerHTML = myjokes[index].joke
+
+
+
+  // Global Variable used to store the quotes  
+// fetched from the API 
+var data; 
+let front = true; 
+  
+// Getting the front and the back author boxes 
+const authors = document.querySelectorAll(".author"); 
+  
+// Getting the front and the back texts 
+const texts = document.querySelectorAll(".text"); 
+  
+// Getting the body 
+const body = document.getElementById("body"); 
+  
+// Getting the buttons 
+const button = document.querySelectorAll(".new-quote"); 
+  
+const blockFront = document.querySelector(".block__front"); 
+const blockBack = document.querySelector(".block__back"); 
+  
+const authorFront = authors[0]; 
+const authorBack = authors[1]; 
+  
+const textFront = texts[0]; 
+const textBack = texts[1]; 
+  
+const buttonFront = button[0]; 
+const buttonBack = button[1]; 
+  
+  
+// An arrow function used to get a quote randomly 
+const displayQuote = () =>{ 
+  
+    // Generates a random number between 0  
+    // and the length of the dataset 
+    let index = Math.floor(Math.random()*data.length); 
+  
+    // Stores the quote present at the randomly generated index 
+    let quote = data[index].text; 
+  
+    // Stores the author of the respective quote 
+    let author = data[index].author; 
+  
+    // Making the author anonymous if no author is present 
+    if(!author){ 
+        author = "Anonymous"
+    } 
+  
+    // Replacing the current quote and the author with a new one 
+  
+    if(front){ 
+        // Changing the front if back-side is displayed 
+        textFront.innerHTML = quote; 
+        authorFront.innerHTML = author; 
+    }else{ 
+        // Changing the back if front-side is displayed 
+        textBack.innerHTML = quote; 
+        authorBack.innerHTML = author; 
+    } 
+      
+    front = !front; 
+  
+} 
+  
+// Fetching the quotes from the type.fit API using promises 
+fetch("https://type.fit/api/quotes") 
+    .then(function(response) { 
+        return response.json();  
+    }) // Getting the raw JSON data 
+    .then(function(data) { 
+  
+        // Storing the quotes internally upon  
+        // successful completion of request 
+        this.data = data;  
+  
+        // Displaying the quote When the Webpage loads 
+        displayQuote()  
+}); 
+  
+  
+// Adding an onclick listener for the button 
+function newQuote(){ 
+      
+    // Rotating the Quote Box 
+    blockBack.classList.toggle('rotateB'); 
+    blockFront.classList.toggle('rotateF'); 
+  
+    // Displaying a new quote when the webpage loads 
+    displayQuote(); 
+}
+
+
+
+
